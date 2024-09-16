ae70d69a5cfe89706fa4c92f64b6be75fea2031f
macrespo42 <macrespo@student.42.fr>
2024-09-16 22:58:02 +0200

    feat: pokecache sublib

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,60 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type CacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cache map[string]CacheEntry
+	mux   *sync.Mutex
+}
+
+func newCache(duration time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]CacheEntry),
+		mux:   &sync.Mutex{},
+	}
+
+	c.reapLoop(duration, time.Now().UTC())
+
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	entry := CacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       val,
+	}
+	c.cache[key] = entry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	entry, exist := c.cache[key]
+	if !exist {
+		return nil, exist
+	}
+	return entry.val, exist
+}
+
+func (c *Cache) reapLoop(duration time.Duration, now time.Time) {
+	ticker := time.NewTicker(duration)
+	for range ticker.C {
+		c.mux.Lock()
+		defer c.mux.Unlock()
+		for key, value := range c.cache {
+			if value.createdAt.Before(now.Add(-duration)) {
+				delete(c.cache, key)
+			}
+		}
+	}
+}
